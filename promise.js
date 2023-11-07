b38f69c32a8f6c83d742ae295b4bb1be392f3753
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-07 16:13:07 +0100

    asyn await

--- promise.js
+++ promise.js
@@ -48,22 +48,73 @@ const api = (time, data, rejectPromise) => {
     });
 }
 
-const firstPromise = api(2_000, 'po 2s', false);
+// const firstPromise = api(2_000, 'po 2s', false);
 
-const secondPromise = api(4_000, 'po 4s', false);
+// const secondPromise = api(4_000, 'po 4s', false);
 
-const thirdPromise = api(3_000, 'po 3s', true);
+// const thirdPromise = api(3_000, 'po 3s', true);
 
-// Czeka na wszystkie obietnice z tablicy
-// Konczy sie sukcesem, jesli wszystkie obietnice zakonczyly sie sukcesem
-// Konczy sie bledem, jesli dowolna obietnica zakonczyla sie bledem
-Promise.all([
-    firstPromise,
-    secondPromise,
-    thirdPromise
-]).then(() => {
-    console.log('Promise all sukces')
-}).catch(() => {
-    console.log('Promise all blad')
-});
+// // Czeka na wszystkie obietnice z tablicy
+// // Konczy sie sukcesem, jesli wszystkie obietnice zakonczyly sie sukcesem
+// // Konczy sie bledem, jesli dowolna obietnica zakonczyla sie bledem
+// Promise.all([
+//     firstPromise,
+//     secondPromise,
+//     thirdPromise
+// ]).then(() => {
+//     console.log('Promise all sukces')
+// }).catch(() => {
+//     console.log('Promise all blad')
+// });
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#static_methods
+
+// const response = api(3_000, 'Sukces', false);
+
+// response.then(data => {
+//     console.log('SUKCES!');
+// });
+
+// Funkcja async/await 
+const fetchData = async () => {
+    try {
+        // Czekamy na zakonczenie obietnicy
+        const response = await api(3_000, 'Sukces', true);
+        
+        // Odpali sie dopiero po zakonczeniu obietnicy
+        console.log('Linijka po obietnicy');
+
+        // Obsluga bledu 
+    } catch(err) {
+        console.log('Blad: ', err);
+    } finally {
+        // ... 
+    }
+
+    // Przyklad z tablica funkcji zwracajacych obietnice, gdzie po kolei wywolujemy kazda z nich
+    // const operations = [
+    //     () => api(3_000, 'Sukces', true),
+    //     () => api(3_000, 'Sukces', true)
+    //   ];
+
+    // for (const operation of operations) {
+    //     try {
+    //         await operation();
+    //     } catch(err) {
+
+    //     }
+    // }
+};
+
+// const fetchData = () => {
+//     const response = api(3_000, 'Sukces', false);
+
+//     response.then(() => {
+//         console.log('Linijka po obietnicy');
+//     });
+// };
+
+fetchData();
+
+console.log('Po Fetch Data');
+
+
