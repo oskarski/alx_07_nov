478e93f4d2ca45b37a0e334f935aebf9dded2fe0
Oskar ALX Kupski <oskar.kupski@gmail.com>
2023-11-07 15:37:28 +0100

    promise continue

--- promise.js
+++ promise.js
@@ -11,7 +11,7 @@ const promise = new Promise((success, reject) => {
     }, 3_000);
 });
 
-console.log(promise);
+// console.log(promise);
 
 // promise.then((data) => {
 //     console.log('Wykonala sie obietnica');
@@ -20,21 +20,50 @@ console.log(promise);
 //     console.log('Blad: ' + error)
 // });
 
-promise
-    .then((data) => {
-        console.log('Wykonala sie obietnica');
-        console.log('Dane ' + data);
+// promise
+//     .then((data) => {
+//         console.log('Wykonala sie obietnica');
+//         console.log('Dane ' + data);
 
-        return 10;
-    })
-    .then(n => {
-        console.log(n);
-    })
-    .catch((error) => {
-        console.log('Blad: ' + error)
-    })
-    .finally(() => {
-        console.log('Zawsze sie wykonam')
+//         return 10;
+//     })
+//     .then(n => {
+//         console.log(n);
+//     })
+//     .catch((error) => {
+//         console.log('Blad: ' + error)
+//     })
+//     .finally(() => {
+//         console.log('Zawsze sie wykonam')
+//     });
+
+// console.log('Po promise');
+
+const api = (time, data, rejectPromise) => {
+    return new Promise((success, reject) => {
+        setTimeout(() => {
+            if (rejectPromise) reject(new Error('API ERROR!'))
+            else success(data);
+        }, time);
     });
+}
 
-console.log('Po promise');
+const firstPromise = api(2_000, 'po 2s', false);
+
+const secondPromise = api(4_000, 'po 4s', false);
+
+const thirdPromise = api(3_000, 'po 3s', true);
+
+// Czeka na wszystkie obietnice z tablicy
+// Konczy sie sukcesem, jesli wszystkie obietnice zakonczyly sie sukcesem
+// Konczy sie bledem, jesli dowolna obietnica zakonczyla sie bledem
+Promise.all([
+    firstPromise,
+    secondPromise,
+    thirdPromise
+]).then(() => {
+    console.log('Promise all sukces')
+}).catch(() => {
+    console.log('Promise all blad')
+});
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#static_methods
